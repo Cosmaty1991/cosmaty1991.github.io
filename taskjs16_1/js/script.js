ca735fd9266306c97797298eee8b150bcd454050
Cosmaty1991 <cosmaty1991@gmail.com>
2016-05-22 13:35:01 +0300

    Initial commit

--- taskjs16_1/js/script.js
+++ taskjs16_1/js/script.js
@@ -1,29 +1,30 @@
 $(function() {
   $('form').submit(function() {
-
-    var urlFull = 'http://api.riffsy.com/v1/search?tag="+value+"&limit=50&output=json&callback=foo';
-
-    $.ajax ({
-      url: urlFull,
-      dataType: 'jsonp',
-
-      success: function(data, textStatus) {
-        var ul = document.createElement('ul');
-
-        $.each(data.results, function(i, val) {
-          var li = document.createElement('li');
-          li.innerHTML = '<a href="' + val.url + '"target="_blank">' + val.title + '</a><p class="url">' + val.url + '</p><p class="content">' + val.content + '</p>';
-          ul.appendChild(li);
-        });
-
-        $('.results').html(ul);
-      }
-    });
-
-    return false;
+    wikiAPI();
   });
-});
 
-function GoogleCallback(func, data) {
-  window[func](data);
-}
+  function wikiAPI(){
+    var value = $('.search').val();
+
+      if (value == '') {
+
+    } else {
+      var urlFull = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + value + '&format=json&callback=?';
+
+      $.ajax({
+        url: urlFull,
+        async: true,
+        dataType: 'jsonp',
+        type: 'GET',
+
+        success: function(data) {
+          $('.results').html('');
+
+          for (var i = 0; i < data.length; i++) {
+            $('.results').prepend("<li class='topic'><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
+          }
+        }
+      });
+    }
+  }
+});
