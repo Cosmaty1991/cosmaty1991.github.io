07d3f12d24a979eaf489b3cc92320f33357ec3dc
Cosmaty1991 <cosmaty1991@gmail.com>
2016-06-13 14:35:23 +0300

    Initial commit

--- taskjs25/js/script.js
+++ taskjs25/js/script.js
@@ -90,11 +90,26 @@ var $grid = $('.section__list').imagesLoaded(function() {
 
 //  ie8
 
-$('[placeholder]').parents('form').submit(function() {
-  $(this).find('[placeholder]').each(function() {
-    var input = $(this);
-    if (input.val() == input.attr('placeholder')) {
-      input.val('');
-    }
-  })
-});
+(function ($) {
+        $.support.placeholder = ('placeholder' in document.createElement('input'));
+    })(jQuery);
+
+
+    //fix for IE7 and IE8
+    $(function () {
+        if (!$.support.placeholder) {
+            $("[placeholder]").focus(function () {
+                if ($(this).val() == $(this).attr("placeholder")) $(this).val("");
+            }).blur(function () {
+                if ($(this).val() == "") $(this).val($(this).attr("placeholder"));
+            }).blur();
+
+            $("[placeholder]").parents("form").submit(function () {
+                $(this).find('[placeholder]').each(function() {
+                    if ($(this).val() == $(this).attr("placeholder")) {
+                        $(this).val("");
+                    }
+                });
+            });
+        }
+    });
