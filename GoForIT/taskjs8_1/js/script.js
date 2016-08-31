2c6f9808d585207d2e9deb5a46544b9c91acf129
Cosmaty1991 <cosmaty1991@gmail.com>
2016-08-31 16:26:53 +0300

    Initial commit

--- GoForIT/taskjs8_1/js/script.js
+++ GoForIT/taskjs8_1/js/script.js
@@ -1,7 +1,8 @@
 $(function() {
   var tabs =  $(".tabs li a");
 
-  tabs.click(function() {
+  tabs.click(function(e) {
+	e.preventDefault();
     var content = this.hash.replace('/', '');
     tabs.removeClass("active");
     $(this).addClass("active");
