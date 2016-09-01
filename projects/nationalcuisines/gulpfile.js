201f9f39a74ba20549dedb81837fe6b600451a78
Cosmaty1991 <cosmaty1991@gmail.com>
2016-09-01 10:53:17 +0300

    Initial commit

--- projects/nationalcuisines/gulpfile.js
+++ projects/nationalcuisines/gulpfile.js
@@ -8,19 +8,19 @@ var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 
 gulp.task('concat', function() {
-  gulp.src(['styles/src/variables.scss', 'styles/src/mixins.scss', 'styles/src/fonts.scss', 'styles/src/reset.scss', 'styles/src/mobile.scss', 'styles/src/tablet.scss', 'styles/src/desktop.scss'])
+  return gulp.src(['styles/src/variables.scss', 'styles/src/mixins.scss', 'styles/src/fonts.scss', 'styles/src/reset.scss', 'styles/src/mobile.scss', 'styles/src/tablet.scss', 'styles/src/desktop.scss'])
       .pipe(concat('main.scss'))
       .pipe(gulp.dest('styles/dest'));
 });
 
 gulp.task('sass', function() {
-  gulp.src('styles/dest/*.scss')
+  return gulp.src('styles/dest/main.scss')
       .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
       .pipe(gulp.dest('styles/dest'));
 });
 
 gulp.task('minify-css', function() {
-  gulp.src('styles/dest/main.css')
+  return gulp.src('styles/dest/main.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('styles/dest'))
     .pipe(rename('main.min.css'))
@@ -39,15 +39,12 @@ gulp.task('compress', function (cb) {
 });
 
 gulp.task('default', function() {
-  gulp.run('concat');
-  gulp.run('sass');
-  gulp.run('minify-css');
-  gulp.run('compress');
+  gulp.run('concat', 'sass', 'minify-css', 'compress');
 });
 
 gulp.task('watch', function() {
   gulp.watch('styles/src/*.scss', ['concat']);
-  gulp.watch('styles/dest/*.scss', ['sass']);
-  gulp.watch('styles/dest/*.scss', ['minify-css']);
-  gulp.watch('js/src/*.scss', ['compress']);
+  gulp.watch('styles/src/*.scss', ['sass']);
+  gulp.watch('styles/dest/*.css', ['minify-css']);
+  gulp.watch('js/src/*.js', ['compress']);
 });
