9fdfe559f82811a661211723369bc932151578d7
Cosmaty1991 <cosmaty1991@gmail.com>
2018-01-12 17:56:31 +0200

    Initial commit

--- projects/happyrelaxhotel/gulpfile.js
+++ projects/happyrelaxhotel/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var cssbeautify = require('gulp-cssbeautify');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
@@ -16,6 +17,16 @@ gulp.task('concat', function() {
 gulp.task('sass', function() {
   return gulp.src('styles/dest/style.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles/dest'));
+});
+
+gulp.task('cssbeautify', function() {
+  return gulp.src('styles/dest/style.css')
+    .pipe(cssbeautify({
+            indent: '  ',
+            openbrace: 'end-of-line',
+            autosemicolon: false
+        }))
     .pipe(gulp.dest('styles/dest'));
 });
 
@@ -44,12 +55,13 @@ gulp.task('compress', function(cb) {
 });
 
 gulp.task('default', function() {
-  gulp.run('concat', 'sass', 'minify-css', 'scripts', 'compress');
+  gulp.run('concat', 'sass', 'cssbeautify', 'minify-css', 'scripts', 'compress');
 });
 
 gulp.task('watch', function() {
   gulp.watch('styles/src/*.scss', ['concat']);
   gulp.watch('styles/src/*.scss', ['sass']);
+  gulp.watch('styles/dest/*.css', ['cssbeautify']);
   gulp.watch('styles/dest/*.css', ['minify-css']);
   gulp.watch('js/src/*.js', ['scripts']);
   gulp.watch('js/dest/*.js', ['compress']);
--- projects/happyrelaxhotel/js/dest/script.js
+++ projects/happyrelaxhotel/js/dest/script.js
@@ -524,24 +524,92 @@ function initMap() {
 
 // search
 
-(function() {
+$(function() {
+  var input = $('.header-search__input');
+  var clear = $('.header-search__clear');
+  var submit = $('.header-search__submit');
+  var content = $('body');
+  var results;
+  var currentClass = 'current';
+  var offsetTop = 50;
+  var currentIndex = 0;
 
-  // create the custom search
 
-  var cx = '017111481218471986513:ucsevlku9cs';
-  var gcse = document.createElement('script');
+  // create function that jump to the element matching the current index
 
-  // make the custom search asynchronous
+  function jumpTo() {
+    if (results.length) {
+      var position;
+      var current = results.eq(currentIndex);
 
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+      results.removeClass(currentClass);
 
-  // add the custom search to website
+      if (current.length) {
+        current.addClass(currentClass);
 
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
-})();
+        position = current.offset().top - offsetTop;
+        window.scrollTo(0, position);
+      }
+    }
+  }
+
+  // create function that search for the entered keyword in the specified context on input
+
+  input.on('input', function() {
+    var searchVal = this.value;
+    content.unmark({
+      done: function() {
+        content.mark(searchVal, {
+          separateWordSearch: true,
+
+          done: function() {
+            results = content.find('mark');
+            currentIndex = 0;
+          },
+
+          noMatch: function() {
+            alert('No results');
+          }
+        });
+      }
+    });
+  });
+
+  // click on the sumbit button to show the result
+
+  submit.on('click', function(e) {
+    e.preventDefault();
+
+    jumpTo();
+  });
+
+  // press the enter button to show the result
+
+  input.on('keypress', function(e) {
+    if (e.which == 13 || e.keyCode == 13) {
+      jumpTo();
+    }
+  });
+
+  // create function that show the clear button when input is active
+
+  input.on('input', function() {
+    clear.fadeIn();
+
+    if (input.val().length === 0) {
+      clear.fadeOut();
+    }
+  });
+
+  // click on the clear button to clear the input
+
+  clear.on('click', function(e) {
+    e.preventDefault();
+
+    content.unmark();
+    input.val('').focus();
+  });
+});
 
 // share
 
