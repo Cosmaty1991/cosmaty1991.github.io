0d14b36ad54430d72027cc786ab02d0f87490679
Cosmaty1991 <cosmaty1991@gmail.com>
2018-01-15 14:09:42 +0200

    Initial commit

--- projects/portfolio/gulpfile.js
+++ projects/portfolio/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var cssbeautify = require('gulp-cssbeautify');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
@@ -9,26 +10,42 @@ var watch = require('gulp-watch');
 
 gulp.task('concat', function() {
   return gulp.src(['styles/src/variables.scss', 'styles/src/mixins.scss', 'styles/src/fonts.scss', 'styles/src/reset.scss', 'styles/src/mobile.scss', 'styles/src/tablet.scss', 'styles/src/desktop.scss'])
-      .pipe(concat('main.scss'))
-      .pipe(gulp.dest('styles/dest'));
-});
-
-gulp.task('sass', function() {
-  return gulp.src('styles/dest/main.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('styles/dest'));
-});
-
-gulp.task('minify-css', function() {
-  return gulp.src('styles/dest/main.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename('main.min.css'))
+    .pipe(concat('style.scss'))
     .pipe(gulp.dest('styles/dest'));
 });
 
-gulp.task('compress', function (cb) {
+gulp.task('sass', function() {
+  return gulp.src('styles/dest/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles/dest'));
+});
+
+gulp.task('cssbeautify', function() {
+  return gulp.src('styles/dest/style.css')
+    .pipe(cssbeautify({
+            indent: '  ',
+            openbrace: 'end-of-line',
+            autosemicolon: false
+        }))
+    .pipe(gulp.dest('styles/dest'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('styles/dest/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('styles/dest'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['js/src/*.js'])
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('js/dest'));
+});
+
+gulp.task('compress', function(cb) {
   pump([
-        gulp.src('js/src/script.js'),
+        gulp.src('js/dest/script.js'),
         uglify(),
         rename('script.min.js'),
         gulp.dest('js/dest')
@@ -38,12 +55,14 @@ gulp.task('compress', function (cb) {
 });
 
 gulp.task('default', function() {
-  gulp.run('concat', 'sass', 'minify-css', 'compress');
+  gulp.run('concat', 'sass', 'cssbeautify', 'minify-css', 'scripts', 'compress');
 });
 
 gulp.task('watch', function() {
   gulp.watch('styles/src/*.scss', ['concat']);
-  gulp.watch('styles/src/*.scss', ['sass']);
+  gulp.watch('styles/dest/*.scss', ['sass']);
+  gulp.watch('styles/dest/*.css', ['cssbeautify']);
   gulp.watch('styles/dest/*.css', ['minify-css']);
-  gulp.watch('js/src/*.js', ['compress']);
+  gulp.watch('js/src/*.js', ['scripts']);
+  gulp.watch('js/dest/*.js', ['compress']);
 });
