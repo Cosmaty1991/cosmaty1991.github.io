e883d60ae71b9a9733a3ec9e5fc88ac5679e69a1
Cosmaty1991 <cosmaty1991@gmail.com>
2018-01-15 14:34:14 +0200

    Initial commit

---
+++ projects/urlaubsgluck/js/src/slider.js
@@ -0,0 +1,27 @@
+// slider
+
+$(function() {
+  $('.main__div').jcarousel();
+
+  $('.main__prev')
+    .on('jcarouselcontrol:active', function() {
+      $(this).removeClass('inactive');
+    })
+    .on('jcarouselcontrol:inactive', function() {
+      $(this).addClass('inactive');
+    })
+    .jcarouselControl({
+      target: '-=1'
+    });
+
+  $('.main__next')
+    .on('jcarouselcontrol:active', function() {
+      $(this).removeClass('inactive');
+    })
+    .on('jcarouselcontrol:inactive', function() {
+      $(this).addClass('inactive');
+    })
+    .jcarouselControl({
+      target: '+=1'
+    });
+});
